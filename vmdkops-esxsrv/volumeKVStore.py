35bd81a1f728ca743df65fb53961ef37a4f1fdeb
Thiruvengada Govindan Thirumal <tthirumal@blr-dbc201.eng.vmware.com>
2016-03-07 02:39:20 -0800

    modified:   vmci_srv.py 	new file:   vol_tests.py 	modified:   volumeKVStore.py

--- vmdkops-esxsrv/volumeKVStore.py
+++ vmdkops-esxsrv/volumeKVStore.py
@@ -2,19 +2,19 @@
 ## This module handles creating and managing a kv store for volumes
 ## (vmdks) created by the docker volume plugin on an ESX host. The
 ## module exposes a set of functions that allow creat/delete/get/set
-## on the kv store. Currently uses side cars to keep KV pairs for 
+## on the kv store. Currently uses side cars to keep KV pairs for
 ## a given volume.
 
 import kvESX
 
 # Default meta-data for a volume created by the plugin, keys can be
-# added or removed during the life of a volume
-defMeta = {'name': 'None',
-           'controller':0,
-           'slot':0,
-           'vmID':1,
-           'status':'detached',
-           'volOpts':'None'};
+# added or removed during the life of a volume. The below list
+# is whats included by default when a kv store is created.
+#
+# 1. status - the status of the volume, whether its attached or
+#             not to a VM.
+# 2. volOpts - the string of options with which the volume was
+#              created.
 
 # Create a kv store object for this volume identified by volPath
 # Create the side car or open if it exists.
@@ -24,13 +24,13 @@ def init():
 
 
 # Create a side car KV store for given volpath
-def create(volPath, name, vm, daemon, status):
-   volMeta = defMeta.copy()
+def create(volPath, status, opts):
+   volMeta = { 'status' : 'detached',
+               'volOpts' : None
+             };
 
-   volMeta['name'] = name
-   volMeta['vmID'] = vm
-   volMeta['daemonID'] = daemon
    volMeta['status'] = status
+   volMeta['volOpts'] = opts
 
    res = kvESX.create(volPath, volMeta)
 
@@ -43,6 +43,16 @@ def create(volPath, name, vm, daemon, status):
 # Delete a kv store object for this volume identified by volPath
 def delete(volPath):
    return kvESX.delete(volPath)
+
+# Get value for a given key (index), returns a string thats the value
+# for the key
+def getAll(volPath):
+   volMeta = kvESX.load(volPath)
+
+   if volMeta:
+      return volMeta
+   else:
+      return None
 
 # Set a string value for a given key(index)
 def set(volPath, key, val):
