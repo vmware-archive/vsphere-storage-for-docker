8f45f4e5f6fb0ac0c26cdd1d67c29fab716c776e
Mark Sterin <msterin@vmware.com>
2016-03-11 16:31:11 -0800

    Added basic logging to Pythoncode (vmci_srv.py)
    This replaces misc prints with simpe logging to /var/log/vmware/docker-vmdk-plugin.log.
    Old location (/tmp/plugin.log) kept in tact just in case there is some sysout
    (It will be dropped when we run server part from VIB/jumpstart. )

--- scripts/startesx.sh
+++ scripts/startesx.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# temporary script to run vmci_srv.py detached.
+# The real log is in "/var/log/vmware/docker-vmdk-plugin.log" (see vmci_srv.py )
+# redirtecting stdio/err to /tmp/plugin.log in case we missed something in logs
+# this will be gone when VIB work is complete
 log=/tmp/plugin.log
-echo ==== `date` ===== > $log
+echo === `date` Actual logs are in /var/log/vmware/docker-vmdk-plugin.log  === > $log
 nohup python /usr/lib/vmware/vmdkops/bin/vmci_srv.py >> $log 2>&1 &
