89f879b78c4bba5f269473594615cce44714dbd1
Ritesh H Shukla <sritesh@vmware.com>
2016-03-15 12:26:16 -0700

    Reset VM logs on drone runs and dump std log

--- scripts/startvm.sh
+++ scripts/startvm.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-log=/tmp/plugin.log
+stdlog=/tmp/plugin.log
+log=/var/log/docker-vmdk-plugin.log
+echo ==== `date` ===== > $stdlog
 echo ==== `date` ===== > $log
 /usr/local/bin/docker-vmdk-plugin < /dev/null >> $log 2>&1 &
