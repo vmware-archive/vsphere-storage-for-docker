f0bf691ecfeb2ba18fc46f622a42199968a19203
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2016-04-28 01:53:12 -0400

    Add auto-resizing of tables for admin cli
    Tables are now resized based on terminal width (discovered via stty)
    
    Move cli_table tests into their own file and expand them to cover column resizing.

--- vmdkops-esxsrv/cli_table.py
+++ vmdkops-esxsrv/cli_table.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-import unittest
-
 # A small module for creating ascii based tables
+
+import subprocess
+
+spaces = 2
 
 def create(header, data):
     """
@@ -27,8 +29,96 @@ def create(header, data):
     Warning: This function does not attempt to fit any fixed width terminal. It
     only formats the data given into columns and does not do any truncation.
     """
-    sizes = max_column_sizes(header, data)
+    max_sizes = max_column_sizes(header, data)
+    ### Subtract the number of spaces between columns from the term width
+    width = term_width() - spaces*(len(header)-1)
+    sizes = shrink_to_fit(max_sizes, width)
+    header = truncate([header], sizes)[0]
+    data = truncate(data, sizes)
     return format(header, data, sizes)
+
+def term_width():
+    """
+    Return the width of the terminal. This function assumes stty exists and is runnable. If that is
+    not the case than a width of 80 characters is returned as a guess/default. This should never
+    happen as stty exists on ESX, Linux and OSX.
+    """
+    try:
+        # Get the number of rows and columns seperated by a space
+        output = subprocess.check_output("stty size".split())
+        return int(output.split()[1])
+    except CalledProcessError:
+        return 80
+
+def shrink_to_fit(column_sizes, term_width):
+    """
+    If the total size of all columns exceeds the terminal width, then we need to shrink the
+    individual column sizes to fit. In most tables, there are one or two columns that are much
+    longer than the other columns. We therefore tailor the shrinking algorithm based on this
+    principle. The algorithm is as follows:
+
+    1) Truncate the longest column until either the columns all fit in the terminal width or
+       the size of the truncated column is equal to the next longest column.
+    2) If the columns fit from truncating we are done.
+    3) If the columns do not fit, shrink the now equally sized columns 1 char at a time until the
+       width fits or these columns equal size of the next smallest column.
+    4) Repeat steps 2 and 3 successively until a fit is found.
+
+    Note that there is the pathological case that the terminal is smaller than a single character
+    for all columns. Ignore this for now. The only way to handle it would be to print every column
+    on it's own line and truncate them to fit. This may be a useful enhancement to make later.
+    """
+    total_size = sum(column_sizes)
+    if total_size <= term_width:
+        return column_sizes
+
+    # Put the columns in sorted order (largest to smallest)
+    sorted_sizes = sorted(column_sizes, reverse=True)
+
+    # Find the index of each sorted column size in the original list and store it
+    # Zero out the value in the original list so that we cover duplicate values, since list.index(val)
+    # only finds the first instance.
+    indexes = []
+    for size in sorted_sizes:
+        index = column_sizes.index(size)
+        indexes.append(index)
+        column_sizes[index] = 0
+
+    # Shrink the sorted columns until they fit the terminal width
+    while total_size > term_width:
+        largest = sorted_sizes[0]
+        num_largest_columns = sorted_sizes.count(largest)
+        if num_largest_columns != len(sorted_sizes):
+            next_largest = sorted_sizes[num_largest_columns]
+        else:
+            # All columns are the same size, so just shrink each one until they fit
+            next_largest = 0
+
+        to_remove = total_size - term_width
+        gap = largest - next_largest
+
+        if gap*num_largest_columns > to_remove:
+            # We can resize in this step and we are done
+            to_remove_per_column = to_remove / num_largest_columns
+            remainder = to_remove % num_largest_columns
+            for i in range(num_largest_columns):
+                sorted_sizes[i] = largest - to_remove_per_column
+            for i in range(remainder):
+                sorted_sizes[i] = sorted_sizes[i] - 1
+        else:
+            # We need to remove the max number of chars until we get to the next largest size then
+            # try again
+            for i in range(num_largest_columns):
+                sorted_sizes[i] = next_largest
+
+        total_size = sum(sorted_sizes)
+
+    # Put the shrunken column sizes in their proper index locations
+    for i in range(len(column_sizes)):
+        index = indexes[i]
+        column_sizes[index] = sorted_sizes[i]
+
+    return column_sizes
 
 def format(header, data, sizes):
     """ Actually create the table as a string """
@@ -42,7 +132,7 @@ def value_row(values, sizes):
     """ Create a one line string of left justified values using the column sizes in sizes """
     s = ''
     for i in range(len(values)):
-        s = s + values[i].ljust(sizes[i]+2)
+        s = s + values[i].ljust(sizes[i]+spaces)
     return s
 
 def divider_row(sizes):
@@ -61,16 +151,23 @@ def max_column_sizes(header, data):
                 sizes[i] = len(row[i])
     return sizes
 
+def truncate(data, sizes):
+    """
+    @param data - A list of a list of values
+    @param sizes - A list of max sizes
 
-class TestTableLogic(unittest.TestCase):
-    """ Test any functions that don't require visual inspection """
-
-    def test_max_column_sizes(self):
-        header = ['Name', 'Greeting']
-        data = [['Bill', 'Hello'],
-                ['Jennifer', 'Sup'],
-                ['Dave', 'How\'s it hangin?']]
-        self.assertEqual([8, 16], max_column_sizes(header, data))
-
-if __name__ == '__main__':
-  unittest.main()
+    Shrink each value in data to it's corresponding max size
+    Show truncation by replacing the last 2 characters with `..`
+    """
+    truncated = []
+    for row in data:
+        truncated_row = []
+        for i in range(len(sizes)):
+            size = sizes[i]
+            column = row[i]
+            if len(column) > size:
+                truncated_row.append(column[:size-2] + '..')
+            else:
+                truncated_row.append(column)
+        truncated.append(truncated_row)
+    return truncated
---
+++ vmdkops-esxsrv/cli_table_test.py
@@ -0,0 +1,84 @@
+# Copyright 2016 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# Tests for cli_table.py
+
+import unittest
+import cli_table
+
+class TestTableLogic(unittest.TestCase):
+    """ Test any functions that don't require visual inspection """
+
+    def test_max_column_sizes(self):
+        header = ['Name', 'Greeting']
+        data = [['Bill', 'Hello'],
+                ['Jennifer', 'Sup'],
+                ['Dave', 'How\'s it hangin?']]
+        self.assertEqual([8, 16], cli_table.max_column_sizes(header, data))
+
+    def test_long_values_shrink_column1(self):
+        """ We only shrink the first column since it's much larger than the second """
+        header = ['Name', 'Some Attribute']
+        data = [['2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                  'blahblahblah'],
+                ['Volume1', 'blah']]
+        sizes = cli_table.max_column_sizes(header, data)
+        self.assertEqual([64, 14], sizes)
+        self.assertEqual([46, 14], cli_table.shrink_to_fit(sizes, 60))
+
+    def test_long_values_shrink_column2(self):
+        """ We only shrink the second column since it's much larger than the first """
+        header = ['a', 'b']
+        data = [['x', '2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b'],
+                ['x', 'afdadsfdasfasfdasdfdasfadsfadsfdas']]
+        sizes = cli_table.max_column_sizes(header, data)
+        self.assertEqual([1, 64], sizes)
+        self.assertEqual([1, 49], cli_table.shrink_to_fit(sizes, 50))
+
+    def test_shrink_multiple_equal_columns(self):
+        """ We have 2 columns of equal sizes so we shrink them equally """
+        header = ['a', 'b']
+        data = [['2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                 '2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b'],
+                ['x', 'afdadsfdasfasfdasdfdasfadsfadsfdas']]
+        sizes = cli_table.max_column_sizes(header, data)
+        self.assertEqual([64, 64], sizes)
+        self.assertEqual([30, 30], cli_table.shrink_to_fit(sizes, 60))
+
+    def test_shrink_2_equal_one_smaller_columns_equally(self):
+        """
+        Since the shrinking required is greater than the reduction of the first 2 columns to the
+        size of the 3rd, we end up shrinking all the columns until they fit.
+        """
+        header = ['a', 'b', 'c']
+        data = [['2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                 '2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                 '2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb2']]
+        sizes = cli_table.max_column_sizes(header, data)
+        self.assertEqual([64, 64, 50], sizes)
+        self.assertEqual([20, 20, 20], cli_table.shrink_to_fit(sizes, 60))
+
+    def test_shrink_2_columns_but_not_3rd(self):
+        """ We can shrink the 2 larger first columns by themselves until they fit and are still
+           larger than the 3rd column """
+        header = ['a', 'b', 'c']
+        data = [['2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                 '2ff76be0906d860c7b7dc79cb6321a3f6a7fb9addf01233eb22a8aa22f096b0b',
+                 '2ff76be0906d860c7b7dc79cb6321a']]
+        sizes = cli_table.max_column_sizes(header, data)
+        self.assertEqual([64, 64, 30], sizes)
+        self.assertEqual([45, 45, 30], cli_table.shrink_to_fit(sizes, 120))
+
+if __name__ == '__main__':
+  unittest.main()
