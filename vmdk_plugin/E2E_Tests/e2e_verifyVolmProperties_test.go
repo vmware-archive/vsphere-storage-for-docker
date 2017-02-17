52376c837f3d001820d71a1cd079ecd0fc0d1b87
ashahi1 <ashahi@vmware.com>
2017-02-17 13:25:45 -0800

    Verify properties (disk-format, capacity, attached-to-vm) of a volume at vm and esx. (#831)
    * Adding E2E test for verifying volume properties

---
+++ vmdk_plugin/E2E_Tests/e2e_verifyVolmProperties_test.go
@@ -0,0 +1,140 @@
+// Copyright 2017 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is an end-to-end test. Test will ssh to a vm and create a volume using docker cli.
+// After creating a volume, test will ssh to the ESX and verify the properties of the volume.
+// Properties being verified - capacity, disk-format and vm-attached field.
+
+// Test assumes that SSH cert has been setup to enable password-less login to VM and ESX.
+
+package e2e_test
+
+import (
+	"github.com/vmware/docker-volume-vsphere/vmdk_plugin/utils"
+	TestUtil "github.com/vmware/docker-volume-vsphere/vmdk_plugin/utils/test_util"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+var (
+	esx              string = os.Getenv("ESX")
+	volSizes                = []string{"100MB"}
+	vms                     = []string{os.Getenv("VM1")}
+	formatTypes             = []string{"thin", "zeroedthick", "eagerzeroedthick"}
+	vmIp             string = ""
+	dockerVolmRmvCmd string = "docker volume rm "
+	dockerCliCheck   bool
+	containerName    string = ""
+)
+
+func TestMain(m *testing.M) {
+	retCode := m.Run()
+	teardownFunction()
+	os.Exit(retCode)
+}
+
+// clean-up function
+func teardownFunction() {
+	TestUtil.ExecCmd(vmIp, dockerVolmRmvCmd)
+	log.Println("-----Clean-up finished - current time: ", time.Now())
+}
+
+/*
+Test steps:
+1. SSH to a vm and create a volume of size 100 mb and specified disk format ["thin", "zeroedthick", "eagerzeroedthick"]
+2. Do docker inspect on the volume and verify size, disk format and attached to vm field.
+   Expected value: {"100MB", "thin/zeroedthick/eagerzeroedthick", no-value} respectively.
+3. SSH to the esx and verify the capacity, disk format and attached-to-vm field for the volume using Admin cli
+   Expected value: {"100MB", "thin/zeroedthick/eagerzeroedthick", "detached"} respectively.
+4. SSH to the vm and run a container and mount the volume created
+5. SSH to the vm and esx and verify the attached-to-vm field for volume - both docker cli and admin cli values should be same.
+6. Again verify capacity and disk format using docker cli and admin cli to make sure things are fine after running the container.
+*/
+func TestVolumeProperties(t *testing.T) {
+	for vmIndx := 0; vmIndx < len(vms); vmIndx++ {
+		log.Println("running end_to_end tests - current time: ", time.Now())
+		vmIp = vms[vmIndx]
+		log.Println("Running test on VM - ", vmIp)
+		dockerCliCheck = TestUtil.IsDockerCliCheckNeeded(vms[vmIndx])
+		for i := 0; i < len(volSizes); i++ {
+			for k := 0; k < len(formatTypes); k++ {
+				containerName = "busybox_" + strconv.FormatInt(time.Now().Unix(), 20)
+				log.Println("Creating a volume of Format Type - ", formatTypes[k])
+				volName := TestInputParamsUtil.GetVolumeNameWithTimeStamp("dockerVol")
+				_, err := TestUtil.InvokeCommand(vms[vmIndx], "docker volume create --driver=vmdk --name="+
+					volName+" -o size="+volSizes[i]+" -o diskformat="+formatTypes[k])
+				if err != nil {
+					log.Fatalf("Failed to create a volume named: %s. Error - %v ", volName, err)
+				} else {
+					log.Println("\n successfully created a  volume -- ", volName)
+					dockerVolmRmvCmd = dockerVolmRmvCmd + " " + volName
+				}
+				log.Println("Verifying volume properties like size, disk-format and attached-to-vm fields "+
+					"at vm and esx for volume - ", volName)
+				volmPropertiesAdminCli := TestUtil.GetVolumePropertiesAdminCli(volName, esx)
+				expctdPropsAdmin := []string{"100MB", formatTypes[k], "detached"}
+				if !hasElement(expctdPropsAdmin, volmPropertiesAdminCli) {
+					log.Fatal("Volume properties on ESX fetched using admin cli does not matches with the expected values")
+				}
+
+				if dockerCliCheck {
+					volmPropertiesDkrCli := TestUtil.GetVolumePropertiesDockerCli(volName, vms[vmIndx])
+					expctdPropsDkr := []string{"100MB", formatTypes[k], "<no value>"}
+					if !hasElement(expctdPropsDkr, volmPropertiesDkrCli) {
+						log.Fatal("Volume properties fetched using docker cli do not matches with the expected values")
+					}
+				}
+				TestUtil.InvokeCommand(vms[vmIndx], "docker run -d -v "+volName+":/vol --name "+containerName+" busybox tail -f /dev/null")
+				// Verifying attached to the vm field for volume
+				vmNameFrmAdminCli := TestUtil.GetVmAttachedToVolUsingAdminCli(volName, esx)
+				if dockerCliCheck {
+					vmNameFrmDockerCli := TestUtil.GetVmAttachedToVolUsingDockerCli(volName, vms[vmIndx])
+					// TODO: get vm name based on ip and compare ith with the docker cli and admin cli
+					if vmNameFrmDockerCli != vmNameFrmAdminCli {
+						log.Fatalf("Information mis-match - Attached-to-VM field for volume from docker cli is [%s]"+
+							"and attched-to-vm field from admin cli is [%s]", vmNameFrmDockerCli, vmNameFrmAdminCli)
+					}
+					volmPropertiesDkrCli := TestUtil.GetVolumePropertiesDockerCli(volName, vms[vmIndx])
+					expctdPropsDkr := []string{"100MB", formatTypes[k]}
+					if !hasElement(expctdPropsDkr, volmPropertiesDkrCli) {
+						log.Fatal("Volume properties on ESX fetched using docker cli do not matches with the expected values")
+					}
+				}
+				volmPropertiesAdminCli = TestUtil.GetVolumePropertiesAdminCli(volName, esx)
+				expctdPropsAdmin = []string{"100MB", formatTypes[k]}
+				if !hasElement(expctdPropsAdmin, volmPropertiesAdminCli) {
+					log.Fatal("Volume properties on admin cli do not matches with the expected values")
+				}
+				log.Println("Finished verifying volume properties like size, disk-format and attached-to-vm fields"+
+					" at vm and esx for volume - ", volName)
+				TestUtil.ExecCmd(vms[vmIndx], "docker stop "+containerName+" ; docker rm "+containerName)
+			}
+		}
+	}
+}
+
+// method checks if the output from the ssh commands contains the expected values
+func hasElement(volmProps []string, op string) bool {
+	for i := 0; i < len(volmProps); i++ {
+		if !strings.Contains(op, volmProps[i]) {
+			log.Printf("Actual values : %s. Does not contains %s ", op, volmProps[i])
+			return false
+		}
+	}
+	return true
+}
---
+++ vmdk_plugin/utils/test_util/VolumePropertiesVerificationUtil.go
@@ -0,0 +1,121 @@
+// Copyright 2017 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This util provides various helper methods that can be used by different tests to
+// fetch information like capacity, disk-format and attched-to-vm fields
+// for volume using docker cli or admin cli.
+
+package e2e
+
+import (
+	"log"
+	"strings"
+)
+
+var ADMIN_CLI_LS = "/usr/lib/vmware/vmdkops/bin/vmdkops_admin.py ls "
+var DOCKER_CLI_INSPC = "docker volume inspect "
+
+// returns attached to vm field of volume using docker cli
+func GetVmAttachedToVolUsingDockerCli(volName string, hostname string) string {
+	cmd := DOCKER_CLI_INSPC + " --format '{{index .Status \"attached to VM\"}}' " + volName
+	op := ExecCmd(hostname, cmd)
+	if op == "" {
+		log.Fatal("Null value is returned by docker cli when looking for attached to vm field for volume. Output: ", op)
+	}
+	return strings.TrimSpace(op)
+}
+
+// returns attached to vm field of volume using admin cli
+func GetVmAttachedToVolUsingAdminCli(volName string, hostname string) string {
+	cmd := ADMIN_CLI_LS + "-c volume,attached-to 2>/dev/null | grep " + volName
+	op := ExecCmd(hostname, cmd)
+	volProps := strings.Fields(op)
+	if op == "" {
+		log.Fatal("Null value is returned by admin cli when looking for attached to vm field for volume. Output: ", op)
+	}
+	if len(volProps) != 2 {
+		log.Fatalf("Admin cli output is expected to consist of two elements only - "+
+			"volume name and attached-to-vm status. Actual output %s ", op)
+	}
+	return volProps[1]
+}
+
+// returns capacity, attached-to-vm and disk-format field
+// for volume using Admin cli
+func GetVolumePropertiesAdminCli(volName string, hostname string) string {
+	cmd := ADMIN_CLI_LS + "-c volume,attached-to,capacity,disk-format 2>/dev/null | grep " + volName
+	op := ExecCmd(hostname, cmd)
+	if op == "" {
+		log.Fatal("Null value is returned by admin cli when looking for, size, disk-format and attached to vm. Output: ", op)
+	}
+	if len(strings.Fields(op)) != 4 {
+		log.Fatalf("Output is expected to consist of four elements only - "+
+			"volume name, attached-to-vm status, size and disk-format. Actual output: %s", op)
+	}
+	return op
+}
+
+// returns capacity,  attached-to-vm and disk-format field
+// for volume using Docker cli
+func GetVolumePropertiesDockerCli(volName string, hostname string) string {
+	cmd := DOCKER_CLI_INSPC + " --format '{{index .Status.capacity.size}} {{index .Status.diskformat}} {{index .Status \"attached to VM\"}}' " + volName
+	op := ExecCmd(hostname, cmd)
+	expctedLen := 0
+	if op == "" {
+		log.Fatal("Null value is returned by docker cli when looking for, size, disk-format and attached to vm. Output: ", op)
+	}
+	// converting the output to an array and comparing the length of array is as expected so that we do not see any
+	// random strings/values attached to the expected output
+	if strings.Contains(op, "<no value>") {
+		expctedLen = 4
+	} else {
+		expctedLen = 3
+	}
+	if len(strings.Fields(op)) != expctedLen {
+		log.Fatalf("Docker cli inpect output is expected to consist of three elements only - "+
+			"size, disk-format and attached-to-vm status. Actual output: %s", op)
+	}
+	return op
+}
+
+// returns docker version
+func GetDockerVersion(hostname string) string {
+	cmd := "docker -v"
+	out := ExecCmd(hostname, cmd)
+	return out
+}
+
+//method takes command and host and calls InvokeCommand
+//and then returns the output after converting to string
+func ExecCmd(hostname string, cmd string) string {
+	out, err := InvokeCommand(hostname, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out[:])
+}
+
+// docker volume inspect on docker 1.11
+// returns less fields as compared to 1.11.
+// So this method can be useful if we
+// do not want to run certain verifications
+// on docker 1.11
+func IsDockerCliCheckNeeded(ipAddr string) bool {
+	dkrVrsn := GetDockerVersion(ipAddr)
+	log.Println("Docker version:  ", dkrVrsn)
+	if strings.Contains(dkrVrsn, "Docker version 1.11.") {
+		return false
+	}
+	return true
+}
