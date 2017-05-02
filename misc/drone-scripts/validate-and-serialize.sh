115bf7d62c9520fa48abeeb7d86ebc36e05dad1f
Nirdesh Shukla <nshukla@vmware.com>
2017-05-02 15:44:34 -0700

    "Restart" CI build (#1209)

--- misc/drone-scripts/validate-and-serialize.sh
+++ misc/drone-scripts/validate-and-serialize.sh
@@ -12,50 +12,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 DS='-ds=datastore1'
 unit_test_array=($TEST_URL_ARRAY)
 numServers=${#unit_test_array[@]}
 DRONE_BUILD_NUMBER=${DRONE_BUILD_NUMBER:=0}
 prevBuildStatus=`drone build info vmware/docker-volume-vsphere $(( $DRONE_BUILD_NUMBER-$numServers ))`
 outArray=($prevBuildStatus)
-total_builds=`drone build list vmware/docker-volume-vsphere| wc -l`
-
-govc datastore.mkdir $DS docker-volume-vsphere/$DRONE_BUILD_NUMBER
-if [ "$?" != "0" ]
-then
-    echo
-    echo
-    echo "Restart not supported"
-    echo "Record already exists for build, push a new commit to trigger build"
-    echo
-    echo
-    exit 1
-fi
-
-# Delete older build records
-govc datastore.rm $DS docker-volume-vsphere/$(( $DRONE_BUILD_NUMBER-$total_builds )) 2>&1 > /dev/null
-
-while [[ ${outArray[2]} == *"running"* ]]; do
-    echo "Waiting 5 minutes for previous build $(( $DRONE_BUILD_NUMBER-$numServers )) to complete";
-    sleep 300;
-    prevBuildStatus=`drone build info vmware/docker-volume-vsphere $(( $DRONE_BUILD_NUMBER-$numServers ))`
-    outArray=($prevBuildStatus)
-done
 
 # Check if any other build is in the ongoing folder, if present, check if entry is stale.
 # If entry is stale clean it up.
 # If entry is an on going build wait for it.
+
 prevBuild=`govc datastore.ls $DS docker-volume-vsphere/ongoing|tail -n 1`
+
 while [[ "$prevBuild" != "" ]];
 do
     prevBuildStatus=`drone build info vmware/docker-volume-vsphere $prevBuild`
     outArray=($prevBuildStatus)
-    if [[ ${outArray[2]} != *"running"* ]]
+    if [[ ${outArray[2]} != *"running"* || ${outArray[2]} == *"running"* && $prevBuild == $DRONE_BUILD_NUMBER ]]
     then
         govc datastore.rm $DS docker-volume-vsphere/ongoing/$prevBuild
     else
-        echo "Waiting 5 minutes for previous build $prevBuild to complete";
-        sleep 300;
+        echo "Waiting 2 minutes for previous build $prevBuild to complete";
+        sleep 120;
     fi
     prevBuild=`govc datastore.ls $DS docker-volume-vsphere/ongoing|tail -n 1`
 done
